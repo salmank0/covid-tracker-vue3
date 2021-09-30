ee355be61010880284c8a3c9151d881df947477f
salmank0 <mohammadsalmank00@gmail.com>
2021-09-30 13:17:09 +0530

    first commit

---
+++ src/components/Header.vue
@@ -0,0 +1,12 @@
+<template>
+  <header class="text-center bg-blue-800 text-white p-4 mb-10">
+    <div class="text-3xl md:text-5xl font-bold mb-3"><i class="fas fa-viruses"></i> Covid Tracker</div>
+    <p>API by <a href="https://covid19api.com" class="text-blue-300" target="_blank">covid19api.com</a></p>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+}
+</script>
